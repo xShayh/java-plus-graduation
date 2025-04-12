46efb268c622b860c34cc1cd38d981cfbd92b06c
Denis585585 <Vegan585@yandex.ru>
2025-04-12 16:58:04 +0300

    Убрал модель событий

--- main-service/src/main/java/ru/practicum/events/mapper/EventMapper.java
+++
@@ -1,4 +0,0 @@
-package ru.practicum.events.mapper;
-
-public interface EventMapper {
-}
--- main-service/src/main/java/ru/practicum/user/mapper/UserMapper.java
+++ main-service/src/main/java/ru/practicum/user/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package ru.practicum.user.mapper;
 
 import org.mapstruct.Mapper;
 import ru.practicum.user.dto.UserDto;
+import ru.practicum.user.dto.UserShortDto;
 import ru.practicum.user.model.User;
 
 @Mapper
@@ -10,4 +11,6 @@ public interface UserMapper {
     UserDto toUserDto(User user);
 
     User toUser(UserDto userDto);
+
+    UserShortDto toUserShortDro(User user);
 }
