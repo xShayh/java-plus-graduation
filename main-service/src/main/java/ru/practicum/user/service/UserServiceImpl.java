2f151072e05aeecc8a7f3b0c5b9e06d9bb31a4d7
Denis585585 <Vegan585@yandex.ru>
2025-04-17 23:31:41 +0300

    Правка чекстайла

--- main-service/src/main/java/ru/practicum/user/service/UserServiceImpl.java
+++ main-service/src/main/java/ru/practicum/user/service/UserServiceImpl.java
@@ -2,7 +2,6 @@ package ru.practicum.user.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
