84dc49a66a7122b9acc896197a1a234802b281aa
Denis585585 <Vegan585@yandex.ru>
2025-04-27 17:44:41 +0300

    Add ErrorHandler on StatsService

--- stat-service/stat-server/src/main/java/ru/practicum/explore/hit/HitRepository.java
+++ stat-service/stat-server/src/main/java/ru/practicum/explore/hit/HitRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.explore.hit;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface HitRepository extends JpaRepository<EndpointHit, Long> {
 }
