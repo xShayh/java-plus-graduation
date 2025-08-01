37cc8c73c167085315837cfa530067875ea8a4c8
shayh <seregaadams20@yandex.ru>
2025-08-01 17:02:30 +0300

    Реализация этапа 1

--- stat-service/stat-client/src/main/java/stat/StatClient.java
+++ stat-service/stat-client/src/main/java/stat/StatClient.java
@@ -4,13 +4,18 @@ import client.BaseClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.MaxAttemptsRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import org.springframework.web.util.UriComponentsBuilder;
-import ru.practicum.dto.EndpointHitDto;
 
+import java.net.URI;
 import java.util.*;
 
 import java.util.List;
@@ -18,17 +23,29 @@ import java.util.List;
 @Service
 @Slf4j
 public class StatClient extends BaseClient {
-    public StatClient(@Value("${server.url}") String statServerUrl, RestTemplateBuilder builder) {
-        super(
-                builder
-                        .uriTemplateHandler(new DefaultUriBuilderFactory(statServerUrl))
-                        .requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
-                        .build()
-        );
-    }
+    private final DiscoveryClient discoveryClient;
+    private final RetryTemplate retryTemplate;
+    private final String statsServiceId;
 
-    public ResponseEntity<Object> saveHit(EndpointHitDto hitDto) {
-        return post("/hit", hitDto);
+    public StatClient(DiscoveryClient discoveryClient,
+                      @Value("${discovery.services.stats-server-id}") String statsServiceId,
+                      RestTemplateBuilder builder) {
+        super(builder
+                .uriTemplateHandler(new DefaultUriBuilderFactory(""))
+                .requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
+                .build());
+
+        this.discoveryClient = discoveryClient;
+        this.statsServiceId = statsServiceId;
+
+        this.retryTemplate = new RetryTemplate();
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(3000L);
+        this.retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+
+        MaxAttemptsRetryPolicy retryPolicy = new MaxAttemptsRetryPolicy();
+        retryPolicy.setMaxAttempts(3);
+        this.retryTemplate.setRetryPolicy(retryPolicy);
     }
 
     public ResponseEntity<Object> getStats(String start, String end, List<String> uris, Boolean unique) {
@@ -58,6 +75,25 @@ public class StatClient extends BaseClient {
         } catch (Exception e) {
             log.info("Error occurred while making the request: {}", e.getMessage(), e);
             throw new RuntimeException("Error during stats request", e);
+        }
+    }
+
+    private URI makeUri(String path) {
+        ServiceInstance instance = retryTemplate.execute(cxt -> getInstance(statsServiceId));
+        log.info("Host() = {} Port() = {}", instance.getHost(), instance.getPort());
+        return URI.create("http://" + instance.getHost() + ":" + instance.getPort() + path);
+    }
+
+    private ServiceInstance getInstance(String serviceId) {
+        try {
+            return discoveryClient
+                    .getInstances(serviceId)
+                    .getFirst();
+        } catch (Exception exception) {
+            throw new RuntimeException(
+                    "Error occurred while detecting stats service address: " + serviceId,
+                    exception
+            );
         }
     }
 }
