7bcf08ca860306a56f075c2faf8948bd1cfd32f8
shayh <seregaadams20@yandex.ru>
2025-08-18 00:12:46 +0300

    fix RequestRepository

--- core/request-service/src/main/java/ru/practicum/repository/RequestRepository.java
+++ core/request-service/src/main/java/ru/practicum/repository/RequestRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.model.Request;
 
@@ -13,9 +12,6 @@ public interface RequestRepository extends JpaRepository<Request, Long> {
 
     List<Request> findAllByRequesterId(Long userId);
 
-    @Query("SELECT r FROM Request r " +
-            "JOIN r.event e " +
-            "WHERE e.id = :eventId AND e.initiator.id = :userId")
     List<Request> findAllByRequesterIdAndEventId(Long userId, Long eventId);
 
     Boolean existsByRequesterIdAndEventId(Long requesterId, Long eventId);
