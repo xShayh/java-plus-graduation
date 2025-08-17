b512614ba4cee76ab9505395c12a9bcd2e3b2234
shayh <seregaadams20@yandex.ru>
2025-08-17 23:30:54 +0300

    add EnableFeignClients to EventApplication

--- core/event-service/src/main/java/ru/practicum/EventApplication.java
+++ core/event-service/src/main/java/ru/practicum/EventApplication.java
@@ -2,8 +2,10 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableFeignClients
 @SpringBootApplication
 @ComponentScan(basePackages = {"stat", "ru.practicum"})
 public class EventApplication {
